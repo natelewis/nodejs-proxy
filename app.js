b1f5c2005ba9bfff1bf94598ae59b32db33a054f
Nate Lewis <Nate.Lewis@endurance.com>
2016-12-20 14:07:49 -0500

    added dynamic protocol detection

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
+// simple proxy example that changes the URL
 let http = require('http');
+let https = require('https');
 let httpProxy = require('http-proxy');
 
-const targetServer = 'http://127.0.0.1:5005';
-const listenPort = 5050;
+// set these ( can be http or https )
+const target = 'https://www.google.com';
+const port = 5050;
 
-// Create a proxy server with custom application logic
-let proxy = httpProxy.createProxyServer({});
+// get the type of requrest, and its host
+var urlArray = target.split('://');
+var type = urlArray[0];
+var host = urlArray[1]; 
 
-// Create server
-let server = http.createServer(function(req, res) {
+// start the server
+httpProxy.createProxyServer({
+  target: target,
+  agent  : global[type + '.globalAgent'],
+  headers: {
+    host: host
+  }
+}).listen(port);
 
-    // Do custom stuff here
-
-    proxy.web(req, res, {target: targetServer});
-
-});
-
-console.log('listening on port ' + listenPort);
-server.listen(listenPort);
+console.log('listening on port ' + port + ' and sending to ' + target);
